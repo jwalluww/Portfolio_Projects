4e05ecc553f49e1f4bb67a85dbecf5a8c74ea37e
Justin Wall <walljd20@gmail.com>
2025-01-18 20:24:54 -0600

    Automated commit at Sat 01/18/2025 20:24:53.98

--- consumer_complaints_dashboard/app.py
+++ consumer_complaints_dashboard/app.py
@@ -8,8 +8,10 @@
 # ============================================================== #
 #%%
 import streamlit as st
-from utils.data_loader import fetch_complaints
 import pandas as pd
+import os
+os.chdir('C:\\Users\\wallj\\DS_Projects\\Portfolio_Projects\\consumer_complaints_dashboard\\utils')
+from data_loader import fetch_complaints
 #%%
 
 # ============================================================== #
@@ -25,40 +27,42 @@ st.sidebar.header("Filters")
 num_complaints = st.sidebar.slider("Number of complaints", 10, 10000, 100)
 fields = st.sidebar.multiselect(
     "Fields to display",
-    options=['product',
-             'complaint_what_happened',
-             'date_sent_to_company',
-             'issue',
-             'sub_product',
-             'zip_code',
-             'tags',
-             'has_narrative',
-             'complaint_id',
-             'timely',
-             'consumer_consent_provided',
-             'company_response',
-             'submitted_via',
-             'company',
-             'date_received',
-             'state',
-             'consumer_disputed',
-             'company_public_response',
-             'sub_issue'],
-    default=["company", "product"]
+    options=['Date received',
+            'Product',
+            'Sub-product',
+            'Issue',
+            'Sub-issue',
+            'Consumer complaint narrative',
+            'Company public response',
+            'Company',
+            'State',
+            'ZIP code',
+            'Tags',
+            'Consumer consent provided?',
+            'Submitted via',
+            'Date sent to company',
+            'Company response to consumer',
+            'Timely response?',
+            'Consumer disputed?',
+            'Complaint ID'],
+    default=["Date received", "Product"]
 )
 
 # Fetch data
 try:
     st.write("Fetching data from API...")
     complaints_data = fetch_complaints(size=num_complaints,fields=fields)
+    # complaints_data = pd.read_csv('C:\\Users\\wallj\\DS_Projects\\Portfolio_Projects\\consumer_complaints_dashboard\\data\\complaints.csv'
+    #                               ,usecols=fields
+    #                               ,nrows=num_complaints)
     st.write("### Complaints Data", complaints_data)
 except Exception as e:
     st.error(f"Error fetching data: {e}")
 
 # Visualization example
 if not complaints_data.empty:
-    st.bar_chart(complaints_data["product"].value_counts())
+    st.bar_chart(complaints_data["Product"].value_counts())
 
 if not complaints_data.empty:
-    st.bar_chart(complaints_data["company"].value_counts())
+    st.bar_chart(complaints_data["Company"].value_counts())
 #%%
--- consumer_complaints_dashboard/utils/data_loader.py
+++ consumer_complaints_dashboard/utils/data_loader.py
@@ -11,14 +11,11 @@ import pandas as pd
 import requests
 #%%
 
-
 # ============================================================== #
-# Hit API for the consumer complaints dataset                    #
+# Read in relevant data from parquet file                        #
 # ============================================================== #
 #%%
-API_URL = "https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/"
-
-def fetch_complaints(fields=["company", "product"], size=10000):
+def fetch_complaints(fields=["Date received", "Product","Company"], size=10000):
     """
     Fetch consumer complaints from the CFPB API.
 
@@ -30,16 +27,41 @@ def fetch_complaints(fields=["company", "product"], size=10000):
     Returns:
         pd.DataFrame: DataFrame of complaints.
     """
-    params = {
-        "size": size,
-    }
-    
-    response = requests.get(API_URL, params=params)
-    if response.status_code == 200:
-        hits = response.json()["hits"]["hits"]
-        data = [hit["_source"] for hit in hits]
-        df = pd.DataFrame(data)
-        df = df[fields]
-    else:
-        raise Exception(f"API Error: {response.status_code}, {response.text}")
+    complaints_data = pd.read_csv('C:\\Users\\wallj\\DS_Projects\\Portfolio_Projects\\consumer_complaints_dashboard\\data\\complaints.csv'
+                                #   ,usecols=fields
+                                  ,nrows=size)
+    return complaints_data
+df = fetch_complaints()
 #%%
+
+# # ============================================================== #
+# # Hit API for the consumer complaints dataset                    #
+# # ============================================================== #
+# #%%
+# API_URL = "https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/"
+
+# def fetch_complaints(fields=["company", "product"], size=10000):
+#     """
+#     Fetch consumer complaints from the CFPB API.
+
+#     Args:
+#         size (int): Number of complaints to fetch.
+#         fields (list): Fields to retrieve.
+#         sort_field (str): Field for sorting.
+
+#     Returns:
+#         pd.DataFrame: DataFrame of complaints.
+#     """
+#     params = {
+#         "size": size,
+#     }
+    
+#     response = requests.get(API_URL, params=params)
+#     if response.status_code == 200:
+#         hits = response.json()["hits"]["hits"]
+#         data = [hit["_source"] for hit in hits]
+#         df = pd.DataFrame(data)
+#         df = df[fields]
+#     else:
+#         raise Exception(f"API Error: {response.status_code}, {response.text}")
+# #%%
