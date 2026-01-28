8bb813de1c31f7eb017c55bfdaba8a1fa1436870
Justin Wall <walljd20@gmail.com>
2026-01-28 05:27:44 -0600

    Automated commit at Wed 01/28/2026  5:27:44.76

--- macro_mlops/src/api/serve_model.py
+++ macro_mlops/src/api/serve_model.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+import threading
+import time
 from pydantic import BaseModel
 import joblib
-import numpy as np
+import os
 import pandas as pd
 from src.utils.db import SessionLocal, PredictionLog # Importing the database session and model
+from src.models.train_model import train_and_save_model  # Ensure the model is trained and saved
 
 app = FastAPI(title="Inflation Predictor API")
 
@@ -23,6 +26,9 @@ class InflationInput(BaseModel):
 def root():
     return {"message": "Inflation prediction API is up and running."}
 
+def load_model():
+    return joblib.load("src/models/model_current.joblib")
+
 @app.post("/predict")
 def predict(input_data: InflationInput):
 
@@ -36,6 +42,7 @@ def predict(input_data: InflationInput):
     ]
 
     features_df = pd.DataFrame([input_data.dict()], columns=columns)
+    model = load_model()
     prediction = model.predict(features_df)[0]
 
     print("🔥 PREDICT CALLED")
@@ -50,3 +57,35 @@ def predict(input_data: InflationInput):
     db.close()
 
     return {"predicted_inflation": float(prediction)}
+
+# We will use a simple global flag to indicate if retraining is in progress
+RETRAINING = False
+
+# Use a secret so not everyone can trigger retraining
+@app.post("/retrain")
+def retrain_model(secret: str):
+    global RETRAINING
+
+    # Simple secret check
+    if secret != os.getenv("RETRAIN_SECRET"):
+        return HTTPException(status_code=403, detail="Unauthorized")
+    
+    # Check if already retraining, avoid concurrent retraining
+    if RETRAINING:
+        return {"status": "Already running"}
+    
+    def retrain_job():
+        global RETRAINING
+        RETRAINING = True
+        
+        metrics = train_and_save_model(output_path="src/models/model_candidate.joblib")
+
+        # Swap!
+        os.replace("src/models/model_candidate.joblib", "src/models/model_current.joblib")
+
+        RETRAINING = False
+    
+    # This will run in a separate thread to avoid blocking the API
+    threading.Thread(target=retrain_job).start()
+
+    return {"status": "Retraining started"}
